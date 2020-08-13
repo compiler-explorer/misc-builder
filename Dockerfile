ad4397eaa14192e913bfa0f29b944b69a9310a32
Matt Godbolt <matt@godbolt.org>
2020-08-13 07:54:15 -0500

    Work in progress on TenDRA (#1)
    * Work in progress on TenDRA
    
    * Upgrades to 20.04 (may cause downstream issues later, but for now...)
    * Installs a bunch of i386 stuff
    * Attempts to build and bootstrap TenDRA
    
    bootstrapping fails, but we're going to play with the native-built part.
    
    Co-authored-by: Patrick Quist <partouf@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,19 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 MAINTAINER Matt Godbolt <matt@godbolt.org>
 
 ARG DEBIAN_FRONTEND=noninteractive
+RUN dpkg --add-architecture i386
 RUN apt update -y -q && apt upgrade -y -q && apt update -y -q && \
     apt install -y -q \
+    bmake \
     curl \
-    make \
     gcc \
+    gcc-multilib \
     git \
+    libc6-dev-i386 \
+    libc6-dev:i386 \
+    linux-libc-dev \
+    make \
     s3cmd \
     xz-utils
 
---
+++ build/buildTenDRA.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+VERSION=$1
+if echo ${VERSION} | grep 'trunk'; then
+    VERSION=trunk-$(date +%Y%m%d)
+    BRANCH=main
+else
+    BRANCH=V${VERSION}
+fi
+
+OUTPUT=/root/tendra-${VERSION}.tar.xz
+S3OUTPUT=""
+if echo $2 | grep s3://; then
+    S3OUTPUT=$2
+else
+    OUTPUT=${2-/root/tendra-${VERSION}.tar.xz}
+fi
+
+PREFIX_BOOTSTRAP=$(pwd)/prefix/bootstrap
+PREFIX_REBUILD=$(pwd)/prefix/rebuild
+
+DIR=$(pwd)/tendra
+git clone --depth 1 -b ${BRANCH} https://github.com/tendra/tendra.git ${DIR}
+
+# no -j (currently breaks)
+pmake -C ${DIR} TARGETARCH=x32_64 LIBCVER=GLIBC2_31 OBJ_BPREFIX=${PREFIX_BOOTSTRAP}
+
+# this seems to ignore libcver somewhere?
+#pmake -C ${DIR} TARGETARCH=x32_64 LIBCVER=GLIBC2_31 OBJ_REBUILD=${PREFIX_REBUILD} bootstrap-rebuild
+
+export XZ_DEFAULTS="-T 0"
+tar Jcf ${OUTPUT} --transform "s,^./,./tendra-${VERSION}/," -C ${PREFIX_BOOTSTRAP} .
+
+if [[ ! -z "${S3OUTPUT}" ]]; then
+    s3cmd put --rr ${OUTPUT} ${S3OUTPUT}
+fi
