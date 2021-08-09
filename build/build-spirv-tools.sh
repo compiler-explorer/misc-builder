47728dd8ad1913ccdf09dc32ed989084bbce6139
Jason Zheng <Jason.Zheng@arm.com>
2021-08-09 13:12:32 +0100

    Add support for SPIR-V Tools (#14)

---
+++ build/build-spirv-tools.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+## $1 : version
+## $2 : destination: a directory or S3 path (eg. s3://...)
+## $3 : last revision successfully build
+
+set -ex
+
+ROOT=$PWD
+VERSION="${1}"
+LAST_REVISION="${3}"
+
+if [[ "${VERSION}" != "master" ]]; then
+    echo "Only support building master"
+    exit 1
+fi
+
+URL="https://github.com/KhronosGroup/SPIRV-Tools.git"
+BRANCH="master"
+
+REVISION=$(git ls-remote --heads "${URL}" "refs/heads/${BRANCH}" | cut -f 1)
+echo "ce-build-revision:${REVISION}"
+
+if [[ "${REVISION}" == "${LAST_REVISION}" ]]; then
+    echo "ce-build-status:SKIPPED"
+    exit
+fi
+
+FULLNAME=SPIRV-Tools-${VERSION}-$(date +%Y%m%d)
+
+OUTPUT=${ROOT}/${FULLNAME}.tar.xz
+S3OUTPUT=
+if [[ $2 =~ ^s3:// ]]; then
+    S3OUTPUT=$2
+else
+    if [[ -d "${2}" ]]; then
+        OUTPUT=$2/${FULLNAME}.tar.xz
+    else
+        OUTPUT=${2-$OUTPUT}
+    fi
+fi
+
+## From now, no unset variable
+set -u
+
+OUTPUT=$(realpath "${OUTPUT}")
+
+git clone --depth 1 "${URL}" --branch "${BRANCH}"
+pushd SPIRV-Tools
+
+./utils/git-sync-deps
+
+mkdir build
+cmake -S . -B build -G "Unix Makefiles"
+cmake --build build
+
+export XZ_DEFAULTS="-T 0"
+tar Jcf "${OUTPUT}" --transform "s,^./,./${FULLNAME}/," ./
+
+if [[ -n "${S3OUTPUT}" ]]; then
+    s3cmd put --rr "${OUTPUT}" "${S3OUTPUT}"
+fi
+popd
