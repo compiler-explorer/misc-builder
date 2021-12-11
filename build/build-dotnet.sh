4198119bef2d2e3aca421f9976e1e0d593d63ec6
Patrick Quist <partouf@gmail.com>
2021-12-11 19:09:18 +0100

    add locale setup code

--- build/build-dotnet.sh
+++ build/build-dotnet.sh
@@ -40,6 +40,12 @@ DIR=$(pwd)/dotnet/runtime
 
 git clone --depth 1 -b ${BRANCH} ${URL} ${DIR}
 cd ${DIR}
+
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen en_US.UTF-8
+
 ./build.sh clr -rc Checked --ninja
 
 tar Jcf ${OUTPUT} .dotnet artifacts/bin/coreclr/Linux.x64.Checked
