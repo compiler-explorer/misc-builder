3ba8bd25c22d7f4b8caac0da98d195e8445b514b
Steve <hez2010@outlook.com>
2021-12-11 18:47:25 +0800

    Add build script for dotnet (#20)

---
+++ build/build-dotnet.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -ex
+
+VERSION=$1
+if echo ${VERSION} | grep 'trunk'; then
+    BRANCH=main
+else
+    BRANCH=release/${VERSION}
+fi
+
+URL=https://github.com/dotnet/runtime.git
+
+FULLNAME=dotnet-${VERSION}.tar.xz
+OUTPUT=${ROOT}/${FULLNAME}
+S3OUTPUT=
+if [[ $2 =~ ^s3:// ]]; then
+    S3OUTPUT=$2
+else
+    if [[ -d "${2}" ]]; then
+        OUTPUT=$2/${FULLNAME}
+    else
+        OUTPUT=${2-$OUTPUT}
+    fi
+fi
+
+DOTNET_REVISION=$(git ls-remote --heads ${URL} refs/heads/${BRANCH} | cut -f 1)
+REVISION="dotnet-${DOTNET_REVISION}"
+LAST_REVISION="${3}"
+
+echo "ce-build-revision:${REVISION}"
+echo "ce-build-output:${OUTPUT}"
+
+if [[ "${REVISION}" == "${LAST_REVISION}" ]]; then
+    echo "ce-build-status:SKIPPED"
+    exit
+fi
+
+DIR=$(pwd)/dotnet/runtime
+
+git clone --depth 1 -b ${BRANCH} ${URL} ${DIR}
+cd ${DIR}
+./build.sh clr -rc Checked --ninja
+
+tar Jcf ${OUTPUT} .dotnet artifacts/bin/coreclr/Linux.x64.Checked
+
+if [[ -n "${S3OUTPUT}" ]]; then
+    aws s3 cp --storage-class REDUCED_REDUNDANCY "${OUTPUT}" "${S3OUTPUT}"
+fi
+
+echo "ce-build-status:OK"
