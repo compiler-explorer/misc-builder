422514dfc062fba4b558eaea0372ec7927c60558
Steve <hez2010@outlook.com>
2021-12-16 20:48:47 +0800

    Move .dotnet to artifacts (#24)

--- build/build-dotnet.sh
+++ build/build-dotnet.sh
@@ -43,7 +43,8 @@ cd ${DIR}
 
 ./build.sh clr -rc Checked --ninja
 
-XZ_OPT=-2 tar Jcf ${OUTPUT} .dotnet artifacts/bin/coreclr/Linux.x64.Checked
+mv .dotnet/ artifacts/bin/coreclr/Linux.x64.Checked/
+XZ_OPT=-2 tar Jcf ${OUTPUT} artifacts/bin/coreclr/Linux.x64.Checked
 
 if [[ -n "${S3OUTPUT}" ]]; then
     aws s3 cp --storage-class REDUCED_REDUNDANCY "${OUTPUT}" "${S3OUTPUT}"
