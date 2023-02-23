1084f5bfb903819d00e379739c3537ad586c0e1a
valomay <valomay@protonmail.com>
2023-02-23 23:50:25 +0100

    update CompCert install script

--- build/build-compcert.sh
+++ build/build-compcert.sh
@@ -1,24 +1,35 @@
-#!/bin/bash 
 
-set -e
+#!/bin/bash
 
-opam init --disasable-sandboxing -n
+set -ex
+
+ROOT=$(pwd)
+VERSION=$(curl -Ss  "https://api.github.com/repos/AbsInt/CompCert/tags" | sed -ne 's/^[ \t]*"name": "//; s/",//p' | head -n 1)
+
+opam init --disable-sandboxing -n
 opam install coq=8.15.2 --yes
 opam install menhir --yes
 eval `opam env`
 
+URL="https://github.com/AbsInt/CompCert.git"
+DIR=${ROOT}/CompCert
 
-VERSION=$(curl -Ss  "https://api.github.com/repos/AbsInt/CompCert/tags" | sed -ne 's/^[ \t]*"name": "//; s/",//p' | head -n 1)
-URL="https://github.com/AbsInt/CompCert/archive/${VERSION}.tar.gz"
+OPT=/opt/compiler-explorer
 
-wget ${URL} 
+rm -rf ${DIR}
+git clone -b ${VERSION} ${URL} ${DIR}
 
-tar zxf ${VERSION}.tar.gz
+cd ${DIR}
 
-cd CompCert-*
-
-./configure x86_64-linux
-make 
-make install
+ARCHS=("ppc" "arm" "rv32" "rv64" "aarch64" "x86_64" "x86_32")
+for ARCH in "${ARCHS[@]}"; do
+    BIN_PREFIX=${OPT}/compcert-${VERSION}/${ARCH}/
+    mkdir -p ${BIN_PREFIX}
+    ./configure ${ARCH}-linux -prefix ${BIN_PREFIX}
+    make
+    make install
+done
 
 echo "ce-build-status:OK"
+
+
