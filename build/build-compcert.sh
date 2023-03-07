7e0bc90a2a5fd8ab6438612b71afe8409943e2c4
Marc Poulhiès <dkm@kataplop.net>
2023-03-07 14:09:01 +0100

    Better handling of incorrect input
    The script would not notice a wrong version.
    Take the opportunity to apply shellcheck.
    
    Signed-off-by: Marc Poulhiès <dkm@kataplop.net>

--- build/build-compcert.sh
+++ build/build-compcert.sh
@@ -5,7 +5,7 @@
 ## $3 : destination: a directory or S3 path (eg. s3://...)
 ## $4 : Last revision successfully build (optional)
 
-set -ex
+set -exu
 
 ROOT=$PWD
 VERSION="${1}"
@@ -14,7 +14,7 @@ LAST_REVISION="${4-}"
 if [[ "${VERSION}" == "master" ]]; then
     VERSION=master-$(date +%Y%m%d)
     BRANCH=heads/master
-else 
+else
     BRANCH="tags/v${VERSION}"
 fi
 
@@ -24,8 +24,8 @@ BASENAME=ccomp-${VERSION}-${ARCH}
 FULLNAME=${BASENAME}.tar.xz
 OUTPUT=${ROOT}/${FULLNAME}
 S3OUTPUT=
-if [[ $3 =~ ^s3:// ]]; then
-    S3OUTPUT=$3
+if [[ "$3" =~ ^s3:// ]]; then
+    S3OUTPUT="$3"
 else
     if [[ -d "${3}" ]]; then
         OUTPUT=$3/${FULLNAME}
@@ -35,6 +35,8 @@ else
 fi
 
 REVISION=$(git ls-remote --tags --heads "${URL}" "refs/${BRANCH}" | cut -f 1)
+[[ -z "$REVISION" ]] && exit 255
+
 echo "ce-build-revision:${REVISION}"
 echo "ce-build-output:${OUTPUT}"
 
@@ -53,13 +55,13 @@ rm -rf "compcert-${ARCH}-${VERSION}"
 git clone "${URL}" "compcert-${ARCH}-${VERSION}"
 
 cd "compcert-${ARCH}-${VERSION}"
-eval `opam env`
+eval "$(opam env)"
 ./configure "${ARCH}-linux" -prefix "${STAGING_DIR}"
 make
 make install
 
 export XZ_DEFAULTS="-T 0"
-tar Jcf ${OUTPUT} --transform "s,^./,./CompCert-${ARCH}-${VERSION}/," -C "${STAGING_DIR}" .
+tar Jcf "${OUTPUT}" --transform "s,^./,./CompCert-${ARCH}-${VERSION}/," -C "${STAGING_DIR}" .
 
 if [[ -n "${S3OUTPUT}" ]]; then
     aws s3 cp --storage-class REDUCED_REDUNDANCY "${OUTPUT}" "${S3OUTPUT}"
