91ca3145b5d8c3beee8a9f25724d432d24be88d1
Matt Godbolt <matt@godbolt.org>
2023-04-23 11:34:38 -0500

    More self review

--- compcert/build.sh
+++ compcert/build.sh
@@ -27,7 +27,7 @@ OUTPUT=$3/${FULLNAME}
 REVISION=$(get_remote_revision "${URL}" "${BRANCH}")
 initialise "${REVISION}" "${OUTPUT}"
 
-skip_if_built "${REVISION}" == "${LAST_REVISION}"
+skip_if_built "${REVISION}" "${LAST_REVISION}"
 
 # The path to compcert will be hardcoded in a config file (share/compcert.ini).
 # This makes the compiler not relocatable without editing this file beforehand.
