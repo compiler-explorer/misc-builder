32c8accbbae257fd8658a258a96d85b3c6fc9fae
Matt Godbolt <matt@godbolt.org>
2023-04-24 22:40:42 -0500

    Port clean, though it's broken currently

--- misc/build-clean.sh
+++ misc/build-clean.sh
@@ -1,51 +1,36 @@
 #!/bin/bash
 
 set -ex
+source common.sh
 
 PROJNAME=clean
 URL=https://gitlab.science.ru.nl/clean-and-itasks/clean-build.git
 
 VERSION=$1
-if echo ${VERSION} | grep 'trunk'; then
-    VERSION=trunk-$(date +%Y%m%d)
-    BRANCH=master
-    LAST_REVISION="${3}"
 
-    CHECKURL2=https://gitlab.science.ru.nl/clean-and-itasks/clean-libraries.git
-    CHECKURL3=https://gitlab.science.ru.nl/clean-compiler-and-rts/compiler.git
-    CHECKURL4=https://gitlab.science.ru.nl/clean-and-itasks/clm.git
-
-    REVISION1=$(git ls-remote --heads "${URL}" "refs/heads/${BRANCH}" | cut -f 1)
-    REVISION2=$(git ls-remote --heads "${CHECKURL2}" "refs/heads/master" | cut -f 1)
-    REVISION3=$(git ls-remote --heads "${CHECKURL3}" "refs/heads/master" | cut -f 1)
-    REVISION4=$(git ls-remote --heads "${CHECKURL4}" "refs/heads/master" | cut -f 1)
-    REVISION=${REVISION1}-${REVISION2}-${REVISION3}-${REVISION4}
-    echo "ce-build-revision:${REVISION}"
-
-    if [[ "${REVISION}" == "${LAST_REVISION}" ]]; then
-        echo "ce-build-status:SKIPPED"
-        exit
-    fi
-else
-    echo "Only builds trunk"
-    exit
+if [[ "${VERSION}" != "trunk" ]]; then
+    echo "Only support building trunk"
+    exit 1
 fi
+
+VERSION=trunk-$(date +%Y%m%d)
+BRANCH=master
+
+CHECKURL2=https://gitlab.science.ru.nl/clean-and-itasks/clean-libraries.git
+CHECKURL3=https://gitlab.science.ru.nl/clean-compiler-and-rts/compiler.git
+CHECKURL4=https://gitlab.science.ru.nl/clean-and-itasks/clm.git
+
+REVISION1=$(get_remote_revision "${URL}" "heads/${BRANCH}")
+REVISION2=$(get_remote_revision "${CHECKURL2}" "heads/master")
+REVISION3=$(get_remote_revision "${CHECKURL3}" "heads/master")
+REVISION4=$(get_remote_revision "${CHECKURL4}" "heads/master")
+REVISION=${REVISION1}-${REVISION2}-${REVISION3}-${REVISION4}
 
 PROJVERSION=${PROJNAME}-${VERSION}
+OUTPUT=$2/${PROJVERSION}.tar.xz
+LAST_REVISION="${3:-}"
 
-OUTPUT=/root/${PROJVERSION}.tar.xz
-S3OUTPUT=""
-if echo $2 | grep s3://; then
-    S3OUTPUT=$2
-else
-    if [[ -d "${2}" ]]; then
-        OUTPUT=$2/${PROJVERSION}.tar.xz
-    else
-        OUTPUT=${2-/root/${PROJVERSION}.tar.xz}
-    fi
-fi
-
-echo "ce-build-output:${OUTPUT}"
+initialise "${REVISION}" "${OUTPUT}" "${LAST_REVISION}"
 
 STARTDIR=$(pwd)
 DIR=${STARTDIR}/${PROJNAME}
@@ -61,12 +46,4 @@ cd ${DIR}
 # download sources and build
 ./clean-classic/linux-x64/build.sh
 
-export XZ_DEFAULTS="-T 0"
-
-tar Jcf ${OUTPUT} --transform "s,^./,./${PROJVERSION}/," -C ${PREFIX} .
-
-if [[ -n "${S3OUTPUT}" ]]; then
-    aws s3 cp --storage-class REDUCED_REDUNDANCY "${OUTPUT}" "${S3OUTPUT}"
-fi
-
-echo "ce-build-status:OK"
+complete "${PREFIX}" "${PROJVERSION}" "${OUTPUT}"
