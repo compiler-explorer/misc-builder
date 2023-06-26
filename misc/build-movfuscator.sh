646a87c65edd644120e5f489f9adf98b7aacf547
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-06-26 08:31:12 -0400

    More movfuscator patching

--- misc/build-movfuscator.sh
+++ misc/build-movfuscator.sh
@@ -32,7 +32,7 @@ cd "${DIR}"
 # Note: Seems to be whitespace-sensitive
 cat > 1.patch << XXX
 diff --git a/movfuscator/host.c b/movfuscator/host.c
-index 36f9b26..6fdc78b 100644
+index 36f9b26..4011233 100644
 --- a/movfuscator/host.c
 +++ b/movfuscator/host.c
 @@ -1,4 +1,5 @@
@@ -41,7 +41,7 @@ index 36f9b26..6fdc78b 100644
  
  #ifndef LCCDIR
  #define LCCDIR "/usr/local/lib/lcc/"
-@@ -50,6 +51,17 @@ char *include[]={
+@@ -50,6 +51,20 @@ char *include[]={
  	0 
  };
  
@@ -49,16 +49,70 @@ index 36f9b26..6fdc78b 100644
 +static void init_include_dirs() {
 +    char buf[256];
 +    strcpy(buf, "-I");
-+    strncat(buf, getenv("MOVINCLUDE"), 255);
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    strncat(buf, "/include", 255);
 +    include[0] = strdup(buf);
++    
 +    strcpy(buf, "-I");
-+    strncat(buf, getenv("MOVGCCINCLUDE"), 255);
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    strncat(buf, "/gcc/include", 255);
 +    include[1] = strdup(buf);
 +}
 +
  char *com[]={
  	LCCDIR "rcc",
  	"-target=x86/mov",
+@@ -59,6 +74,15 @@ char *com[]={
+ 	0
+ };
+ 
++__attribute__((constructor))
++static void init_com() {
++    char buf[256];
++    strcpy(buf, "-I");
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    strncat(buf, "/include", 255);
++    include[0] = strdup(buf);
++}
++
+ char *as[]={
+ 	"/usr/bin/as",
+ 	"--32",
+@@ -90,6 +114,34 @@ char *ld[]={
+ 	0
+ };
+ 
++__attribute__((constructor))
++static void init_ld() {
++    char buf[256];
++    strcpy(buf, "-L");
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    include[5] = strdup(buf);
++    
++    strcpy(buf, "-L");
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    strncat(buf, "/gcc/32", 255);
++    include[6] = strdup(buf);
++    
++    strcpy(buf, "");
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    strncat(buf, "/crt0.o", 255);
++    include[10] = strdup(buf);
++    
++    strcpy(buf, "");
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    strncat(buf, "/crtf.o", 255);
++    include[13] = strdup(buf);
++    
++    strcpy(buf, "");
++    strncat(buf, getenv("MOVBUILDDIR"), 255);
++    strncat(buf, "/crtd.o", 255);
++    include[14] = strdup(buf);
++}
++
+ int option(char* arg) 
+ {
+ 	if (strcmp(arg, "-g")==0) {
 XXX
 
 git apply 1.patch
