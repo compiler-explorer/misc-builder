0dd6ad3e6c59213a3e1cb2519ff2c23343fbbad2
Xavier Routh <xavierrouth@gmail.com>
2023-08-21 21:25:35 -0500

    Add C-LC3 Compiler Build script (#67)
    * lc3 compiler build script
    
    ---------
    
    Co-authored-by: Xavier Routh <xrouth2@illinois.edu>

---
+++ misc/build-lc3.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+source common.sh
+
+VERSION=$1
+if [[ "${VERSION}" = "trunk" ]]; then
+    VERSION=trunk-$(date +%Y%m%d)
+    BRANCH=main
+else
+    BRANCH=V${VERSION}
+fi
+
+URL=https://github.com/xavierrouth/C-LC3-Compiler
+
+FULLNAME=C-LC3-Compiler-${VERSION}.tar.xz
+OUTPUT=$2/${FULLNAME}
+
+REVISION="C-LC3-Compiler-$(get_remote_revision "${URL}" "heads/${BRANCH}")"
+LAST_REVISION="${3:-}"
+
+initialise "${REVISION}" "${OUTPUT}" "${LAST_REVISION}"
+
+PREFIX=$(pwd)/prefix
+DIR=$(pwd)/C-LC3-Compiler
+BUILD=${DIR}/build
+
+git clone --recurse-submodules --depth 1 -b "${BRANCH}" "${URL}" "${DIR}"
+
+mkdir "${BUILD}"
+cd "${BUILD}"
+cmake ..
+make
+mkdir -p "${PREFIX}"
+
+cp "${BUILD}/lc3-compile" "${PREFIX}"
+
+complete "${PREFIX}" "C-LC3-Compiler-${VERSION}" "${OUTPUT}"
