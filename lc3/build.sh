5ab0735c1583fb5d67c0f05088b0a837961576c0
Xavier Routh <xrouth2@illinois.edu>
2023-08-31 00:00:09 -0500

    updated lc3-compiler build script

---
+++ lc3/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+source common.sh
+export PATH="$PATH":"$HOME"/.cargo/bin
+source $HOME/.cargo/env
+
+VERSION=$1
+if [[ "${VERSION}" = "trunk" ]]; then
+    VERSION=trunk-$(date +%Y%m%d)
+    BRANCH=main
+else
+    BRANCH=V${VERSION}
+fi
+
+URL=https://github.com/xavierrouth/lc3-compiler
+
+FULLNAME=lc3-compiler-${VERSION}.tar.xz
+OUTPUT=$2/${FULLNAME}
+
+REVISION="lc3-compiler-$(get_remote_revision "${URL}" "heads/${BRANCH}")"
+LAST_REVISION="${3:-}"
+
+initialise "${REVISION}" "${OUTPUT}" "${LAST_REVISION}"
+
+PREFIX=$(pwd)/prefix
+DIR=$(pwd)/lc3-compiler
+BUILD=${DIR}/target/release
+
+git clone --depth 1 -b "${BRANCH}" "${URL}" "${DIR}"
+
+cd $DIR
+cargo build --release
+mkdir -p "${PREFIX}"
+
+cp "${BUILD}/lc3-compile" "${PREFIX}"
+
+complete "${PREFIX}" "lc3-compiler-${VERSION}" "${OUTPUT}"
