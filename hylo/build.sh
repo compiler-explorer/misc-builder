42c40105cb405594b9b8ff867fff3dff24f6d071
nickpdemarco <nickpdemarco@gmail.com>
2023-10-11 17:10:12 -0400

    Clean up build script

--- hylo/build.sh
+++ hylo/build.sh
@@ -2,43 +2,42 @@
 
 ## $1 : version
 ## $2 : destination: a directory
+## $3 : last revision: a revision descriptor which may be fetched from the cache.
 
-set -eu
+set -exu
 source common.sh
 
-VERSION="${1}"
-LAST_REVISION="${3-}"
-
+ROOT=$(pwd)
+VERSION=$1
 URL="https://github.com/hylo-lang/hyloc"
 
 if echo "${VERSION}" | grep 'trunk'; then
-   VERSION=trunk-$(date +%Y%m%d)
-   BRANCH=main
-   REVISION=$(get_remote_revision "${URL}" "heads/${BRANCH}")
+    VERSION=trunk-$(date +%Y%m%d)
+    BRANCH=main
+    REVISION=$(get_remote_revision "${URL}" "heads/${BRANCH}")
 else
-   BRANCH=v${VERSION}
-   REVISION=$(get_remote_revision "${URL}" "tags/${BRANCH}")
+    BRANCH=${VERSION}
+    REVISION=$(get_remote_revision "${URL}" "tags/${BRANCH}")
 fi
 
-FULLNAME=hylo-${VERSION}
-OUTPUT=$2/hylo/${FULLNAME}.tar.xz
-mkdir "$(dirname ${OUTPUT})"
+FULLNAME=hylo-${VERSION}.tar.xz
+OUTPUT=${ROOT}/${FULLNAME}
+LAST_REVISION="${3:-}"
+
+if [[ -d "${2}" ]]; then
+   OUTPUT=$2/${FULLNAME}
+else
+   OUTPUT=${2-$OUTPUT}
+fi
 
 initialise "${REVISION}" "${OUTPUT}" "${LAST_REVISION}"
 
-OUTPUT=$(realpath "${OUTPUT}")
+rm -rf "hylo-${VERSION}"
+git clone -q --depth 1 --single-branch -b "${BRANCH}" "${URL}" "hylo-${VERSION}"
 
-BUILD_DEST=/opt/compiler-explorer/hylo/hylo-${VERSION}
-mkdir -p "$(dirname "${BUILD_DEST}")"
-git clone "${URL}" --depth=1 "--branch=${BRANCH}" "${BUILD_DEST}"
-
-pushd "${BUILD_DEST}"
-
+cd "hylo-${VERSION}"
 swift package resolve
 .build/checkouts/Swifty-LLVM/Tools/make-pkgconfig.sh /usr/local/lib/pkgconfig/llvm.pc
-export PKG_CONFIG_PATH=$PWD:/usr/lib/x86_64-linux-gnu/
 swift build -c release
 
-complete .build/release/* "${FULLNAME}" "${OUTPUT}"
-
-popd
+complete .build/release/ "${FULLNAME}" "${OUTPUT}"
