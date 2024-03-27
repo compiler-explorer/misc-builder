797d08389c43ec21fbd1b94eb8cff5bab85d8af8
partouf <partouf@gmail.com>
2024-03-27 18:02:09 +0100

    also build mad assembler

--- mads/build.sh
+++ mads/build.sh
@@ -6,33 +6,49 @@ source common.sh
 VERSION=$1
 if [[ "${VERSION}" = "trunk" ]]; then
     VERSION=trunk-$(date +%Y%m%d)
-    BRANCH=master
+    BRANCH1=master
+    BRANCH2=master
 else
-    BRANCH=v${VERSION}
+    echo "Versioned build not supported"
+    return 1
 fi
 
-URL=https://github.com/tebe6502/Mad-Pascal
+URL1=https://github.com/tebe6502/Mad-Assembler
+URL2=https://github.com/tebe6502/Mad-Pascal
 
 FULLNAME=madpas-compiler-${VERSION}.tar.xz
 OUTPUT=$2/${FULLNAME}
 
-REVISION="madpas-compiler-$(get_remote_revision "${URL}" "heads/${BRANCH}")"
+REVISION1="$(get_remote_revision "${URL1}" "heads/${BRANCH1}")"
+REVISION2="$(get_remote_revision "${URL2}" "heads/${BRANCH2}")"
+
+REVISION="madpas-compiler-${REVISION1}-${REVISION2}"
 LAST_REVISION="${3:-}"
 
 initialise "${REVISION}" "${OUTPUT}" "${LAST_REVISION}"
 
 PREFIX=$(pwd)/prefix
-DCU=$(pwd)/dcu
-DIR=$(pwd)/madpas-compiler
 
-git clone --depth 1 -b "${BRANCH}" "${URL}" "${DIR}"
+DCU1=$(pwd)/dcu1
+DCU2=$(pwd)/dcu2
 
-mkdir -p "${DCU}"
+DIR1=$(pwd)/madpas-assembler
+DIR2=$(pwd)/madpas-compiler
+
+mkdir -p "${DCU1}"
+mkdir -p "${DCU2}"
+
 mkdir -p "${PREFIX}/bin"
 
-cd $DIR
+git clone --depth 1 -b "${BRANCH1}" "${URL1}" "${DIR1}"
 
-/opt/compiler-explorer/fpc-3.2.2.x86_64-linux/bin/fpc @/opt/compiler-explorer/fpc/fpc.cfg -Mdelphi -vh -O3 "-FE${PREFIX}/bin" "-FU${DCU}" src/mp.pas
+cd "$DIR1"
+/opt/compiler-explorer/fpc-3.2.2.x86_64-linux/bin/fpc @/opt/compiler-explorer/fpc/fpc.cfg -Mdelphi -vh -O3 "-FE${PREFIX}/bin" "-FU${DCU1}" mads.pas
+
+git clone --depth 1 -b "${BRANCH2}" "${URL2}" "${DIR2}"
+
+cd "$DIR2"
+/opt/compiler-explorer/fpc-3.2.2.x86_64-linux/bin/fpc @/opt/compiler-explorer/fpc/fpc.cfg -Mdelphi -vh -O3 "-FE${PREFIX}/bin" "-FU${DCU2}" src/mp.pas
 
 cp -Rf base "${PREFIX}/base"
 cp -Rf lib "${PREFIX}/lib"
