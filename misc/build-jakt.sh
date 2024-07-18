c6a121fa5cc1a464e6073462f02c4d79413b4489
Patrick Quist <partouf@gmail.com>
2024-07-18 11:21:03 +0200

    More symlink fixes

--- misc/build-jakt.sh
+++ misc/build-jakt.sh
@@ -74,7 +74,9 @@ mv ../runtime "${STAGING_DIR}/"
 mv ../.clang-format "${STAGING_DIR}/"
 
 rm "${STAGING_DIR}/bin/jakt"
+rm "${STAGING_DIR}/include/runtime"
 ln -sr "${STAGING_DIR}/bin/jakt-stage1" "${STAGING_DIR}/bin/jakt"
+ln -sr "${STAGING_DIR}/runtime" "${STAGING_DIR}/include/runtime"
 
 export XZ_DEFAULTS="-T 0"
 tar Jcf "${OUTPUT}" --transform "s,^./,./${SUBDIR}/," -C "${STAGING_DIR}" .
